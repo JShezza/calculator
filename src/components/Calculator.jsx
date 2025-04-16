64328848ad7d45731234db606500d78ad053b23a
JShezza <shezzalord@gmail.com>
2025-04-16 08:52:08 +0100

    getButtonClass helepr function

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,7 +1,6 @@
 import './Calculator.css';
 
 function CreateCalculator() {
-	
 	//button values for Button component
 	const buttonValues = [
 		['%', 'CE', 'C', '←'],
@@ -11,6 +10,29 @@ function CreateCalculator() {
 		['1', '2', '3', '+'],
 		['⁺⁄₋', '0', '.', '='],
 	];
+
+	/**
+	 * As we are setting the buttonValues to their classname
+	 * I want a helper function to get the classname for each button
+	 * to make it easier to style them in the CSS file.
+	 */
+	const getButtonClass = (button) => {
+		//Numbers
+		if (!isNaN(button) && button != '') {
+			// console.log(`${button} is a number`);
+			return 'number';
+		} else if (
+			['%', 'CE', 'C', '←', '⅟x', '𝑥²', '√𝑥', '÷', '×', '–', '+'].includes(
+				button
+			)
+		) {
+			// console.log(`${button} is an operation`);
+			return 'operations';
+		} else if (button === '=') {
+			// console.log(`${button} is equals`);
+			return 'equals';
+		}
+	};
 
 	//Button component to render buttonValues
 	const Button = ({ className, value, onClick }) => {
@@ -25,19 +47,18 @@ function CreateCalculator() {
 		<div className='wrapper'>
 			<div className='screen'></div>
 			<div className='buttonBox'>
-					{buttonValues.flat().map((button, i) => {
-						return (
-							<Button
-								className={button === '=' ? 'equals' : ''}
-								value={button}
-								key={i}
-								onClick={() => {
-									console.log(`${i} Clicked!`);
-								}}
-							/>
-						);
-					})}
-				
+				{buttonValues.flat().map((button, i) => {
+					return (
+						<Button
+							className={getButtonClass(button)}
+							value={button}
+							key={i}
+							onClick={() => {
+								console.log(`${button} Clicked!`);
+							}}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
